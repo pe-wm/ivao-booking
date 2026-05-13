553602048464c4cbdc78baef07da7d0098f58879
IVAO PERU <pe-wm@ivao.aero>
2026-05-13 07:46:05 +0200

    Statistics now separating prebooked from booked flights

--- inc/statistics.php
+++ inc/statistics.php
@@ -9,11 +9,12 @@
 
 /**
  * Returns a progress bar according to the given parametres
+ * @param int $prebooked
  * @param int $booked
  * @param int $all
  * @return string HTML
  */
-function progressBar($booked, $all)
+function progressBar($prebooked, $booked, $all)
 {
 	if ($all < 1)
 	{
@@ -23,12 +24,14 @@ function progressBar($booked, $all)
 	}
 	else
 	{
-		$percentBooked = $booked / $all * 100; 
-		$percentFree = 100 - $percentBooked;
+		$percentPrebooked = $prebooked / $all * 100;
+		$percentBooked = $booked / $all * 100;
+		$percentFree = 100 - $percentPrebooked - $percentBooked;
 		return '
 			<div class="progress" style="height: 1.5rem">
-				<div class="progress-bar progress-bar-striped progress-bar-animated bg-danger" role="progressbar" style="width: ' . $percentBooked . '%" aria-valuenow="' . $percentBooked . '" aria-valuemin="0" aria-valuemax="100">' . round($percentBooked, 2) . '%</div>
-				<div class="progress-bar progress-bar-striped progress-bar-animated bg-success" role="progressbar" style="width: ' . $percentFree . '%" aria-valuenow="' . $percentFree . '" aria-valuemin="0" aria-valuemax="100">' . round($percentFree, 2) . '%</div>
+				<div class="progress-bar progress-bar-striped progress-bar-animated bg-warning" role="progressbar" style="width: ' . $percentPrebooked . '%" aria-valuenow="' . $percentPrebooked . '" aria-valuemin="0" aria-valuemax="100">' . ($percentPrebooked > 4 ? round($percentPrebooked, 2) . '%' : '') . '</div>
+				<div class="progress-bar progress-bar-striped progress-bar-animated bg-danger" role="progressbar" style="width: ' . $percentBooked . '%" aria-valuenow="' . $percentBooked . '" aria-valuemin="0" aria-valuemax="100">' . ($percentBooked > 4 ? round($percentBooked, 2) . '%' : '') . '</div>
+				<div class="progress-bar progress-bar-striped progress-bar-animated bg-success" role="progressbar" style="width: ' . $percentFree . '%" aria-valuenow="' . $percentFree . '" aria-valuemin="0" aria-valuemax="100">' . ($percentFree > 4 ? round($percentFree, 2) . '%' : '') . '</div>
 			</div>';
 	}
 }
@@ -46,9 +49,10 @@ function statisticLine($airport = null)
 	{
 		$result .= '<h2>Overall statistics</h2>';
 		$stat = EventAirport::getStatisticsAll();
-		$booked = $stat["prebooked"] + $stat["booked"];
+		$prebooked = $stat["prebooked"];
+		$booked = $stat["booked"];
 		$free = $stat["free"];
-		$result .= progressBar($booked, $free + $booked);
+		$result .= progressBar($prebooked, $booked, $free + $prebooked + $booked);
 	}
 	else
 	{
@@ -59,20 +63,24 @@ function statisticLine($airport = null)
 			
 		$result .= ' <a href="flights#' . $airport->icao . '">' . $airport->icao . '</a> <small>' . $airport->name . '</small></h2>';
 		$stat = $airport->getStatistics();
-		$booked = $stat["prebooked"] + $stat["booked"];
+		$prebooked = $stat["prebooked"];
+		$booked = $stat["booked"];
 		$free = $stat["free"];
-		$result .= progressBar($booked, $free + $booked);
+		$result .= progressBar($prebooked, $booked, $free + $prebooked + $booked);
 	}
 	$result .= '
-			<div class="row">				
-				<div class="col-sm-4 h5 text-center">
+			<div class="row">
+				<div class="col-sm-3 h5 text-center">
+					Prebooked: <span class="badge badge-warning">' . $prebooked . '</span>
+				</div>
+				<div class="col-sm-3 h5 text-center">
 					Booked: <span class="badge badge-danger">' . $booked . '</span>
 				</div>
-				<div class="col-sm-4 h5 text-center">
+				<div class="col-sm-3 h5 text-center">
 					Free: <span class="badge badge-success">' . $free . '</span>
 				</div>
-				<div class="col-sm-4 h5 text-center">
-					All flights: <span class="badge badge-primary">' . ($free + $booked) . '</span>
+				<div class="col-sm-3 h5 text-center">
+					All flights: <span class="badge badge-primary">' . ($free + $prebooked + $booked) . '</span>
 				</div>
 			</div>
 		</div>';
